abd1f77735e9005865971a316208c58310a0106a
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-01-22 16:51:48 -0500

    Updating all works

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function myFunction(x,y, result){
+  console.log(result(x,y));
+}
+function add(a,b) {
+  return(a+b);
+}
+function sub(a,b) {
+  return(a-b);
+}
+function mult(a,b) {
+  return(a*b);
+}
+myFunction( 6, 4, add);
+myFunction( 9, 4, sub);
+myFunction( 12, 4, mult);
+
+
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  // Return a function that when invoked increments and returns
+  // a counter variable.
+  let count = 0;
+  return function () {
+    return ++count;
+  }
 };
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+//newCounter(); // 1
+//newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -17,4 +40,14 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let increment = 0;
+  let decrement = 0;
+  return {
+    incrementing : function () {
+      return ++increment;
+    },
+    decrementing: function () {
+      return --decrement;
+    }
+  }
 };
